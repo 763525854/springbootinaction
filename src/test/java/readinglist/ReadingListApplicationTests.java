8b296d13476ef2fb0cf53b656d289180d4286b8e
rhyme <763525854@qq.com>
2018-05-02 11:29:46 +0800

    提交空的测试类

---
+++ src/test/java/readinglist/ReadingListApplicationTests.java
@@ -0,0 +1,24 @@
+/**
+ *  Created by weiping.gong on 2018年5月2日
+ */
+package readinglist;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+/**
+ * @Author: weiping.gong
+ * @Description:
+ * @Date: created in 2018年5月2日
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = ReadingListApplication.class)
+@WebAppConfiguration
+public class ReadingListApplicationTests {
+	@Test
+	public void contextLoads() {
+	}
+}
