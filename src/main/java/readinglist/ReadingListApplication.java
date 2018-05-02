802fe71bf3ca58c2594b875c3e780db5b1d125ab
rhyme <763525854@qq.com>
2018-05-02 11:26:10 +0800

    第一次提交

---
+++ src/main/java/readinglist/ReadingListApplication.java
@@ -0,0 +1,19 @@
+/**
+ *  Created by weiping.gong on 2018年5月2日
+ */
+package readinglist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author: weiping.gong
+ * @Description:
+ * @Date: created in 2018年5月2日
+ */
+@SpringBootApplication
+public class ReadingListApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(ReadingListApplication.class, args);
+	}
+}
